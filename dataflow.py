ffe65ff6f26b2d978f0a15f971fdbc126a142f1f
Randy Smith <git-general@tigana.org>
2009-11-28 23:04:05 -0500

    Added ability to shutdown nodes when they know they aren't going to do anymore, and the propagate that upstream.  Did that for the interval nodes.

--- dataflow.py
+++ dataflow.py
@@ -33,6 +33,12 @@ import operator
 ### (possibly mimic building a list; stream DFN container?  Any good 
 ### syntactic sugar for splits?)
 
+### XXX: I need a clearer distinction in states between "figuring out
+### linkages" and "flowing".  I need to know whether I can trust
+### the linkage info.
+
+### XXX: Why am I assuming a single input before inputs get attached?
+
 class DataflowNode(object):
     """Base class for node in a dataflow network.  Takes an input record,
 does some type of transformation on it, and outputs some other record.  
@@ -46,7 +52,9 @@ descendants."""
         # Default to a single input.  If there are more from other DFNs,
         # the array will expand automatically, and it currently doesn't
         # make sense to have no inputs for a DFN.  
-        self.eosSeen = [False,]  
+        self.upstreamInfo = [] # Tuples of obj, output#
+        self.eosSeen = [False,]
+        self.shutdown = False
 
     # input and eos are both called by both user and internal links
     def input(self, record, inputLink=0):
@@ -94,21 +102,40 @@ Descendants may override to get this notification."""
             if self.outputFunctions[l]:
                 self.outputFunctions[l](record)
 
+    def _shutdown(self):
+        """Inform upstream nodes that we're going away and they shouldn't
+        bother us anymore.  Note that this is independent from sending
+        eos downstream."""
+        self.shutdown = True
+        for usn in self.upstreamInfo:
+            (node, port) = usn
+            node._breakPipe(port)
+
+    def _breakPipe(self, port):
+        self.outputFunctions[port] = None
+        self.outputEos[port] = None
+        if not filter(None, self.outputFunctions):
+            # We're done; we've got no more customers
+            self._shutdown()
+
     @staticmethod
     def link(outputNode, inputNode, outputLink=0, inputLink=0):
-        assert outputNode._validate_link(outputLink, False)
-        assert inputNode._validate_link(inputLink, True)
+        assert outputNode._validate_link(outputLink, False), (outputNode, outputLink)
+        assert inputNode._validate_link(inputLink, True), (inputNode, inputLink)
         outputNode.outputFunctions += \
             [None,] * max(0,outputLink - len(outputNode.outputFunctions) + 1)
         outputNode.outputEos += \
             [None,] * max(0,outputLink - len(outputNode.outputEos) + 1)
         inputNode.eosSeen += \
             [None,] * max(0,inputLink - len(inputNode.eosSeen) + 1)
+        inputNode.upstreamInfo  += \
+            [None,] * max(0,inputLink - len(inputNode.upstreamInfo) + 1)
         outputNode.outputFunctions[outputLink] = \
             lambda record: inputNode.input(record, inputLink=inputLink)
         outputNode.outputEos[outputLink] = \
             lambda: inputNode.eos(inputLink=inputLink)
         inputNode.eosSeen[inputLink] = False
+        inputNode.upstreamInfo[inputLink] = (outputNode, outputLink)
 
 # Utility dataflow classes
 class StreamDFN(DataflowNode):
@@ -157,9 +184,9 @@ class SplitDFN(DataflowNode):
     def input(self, record, inputLink=0):
         self._output(record)
 
-    def addOutput(self, downstreamNode, outputLink=0):
-        DataflowNode.link(self, downstreamNode, outputLink,
-                          self._firstOpenOutput())
+    def addOutput(self, downstreamNode, downstreamlink=0):
+        DataflowNode.link(self, downstreamNode, self._firstOpenOutput(),
+                          downstreamlink)
 
 class FilterDFN(DataflowNode):
     """Filters input through a specified function."""
@@ -203,6 +230,9 @@ class RecordIntervalDFN(DataflowNode):
             and (self.interval[1] == -1 or self.recordNum < self.interval[1])):
             self._output(record)
         self.recordNum += 1
+        if self.recordNum >= self.interval[1]:
+            self.eos()
+            self._shutdown()
 
 class ByteIntervalDFN(DataflowNode):
     """Only transmit a specified byte interval (where input/output is in text strings)."""
@@ -219,12 +249,20 @@ class ByteIntervalDFN(DataflowNode):
 
     def input(self, record, inputLink=0):
         strlen = len(record)
-        startInStr = max(0, self.interval[0] - self.byteNum)
-        endInStr = min(strlen, self.interval[1] - self.byteNum) if self.interval[1] != -1 else strlen
+
+        # Map the byte interval into the string coords
+        # Limit by string boundaries
+        startInStr = self.interval[0] - self.byteNum
+        startInStr = min(strlen, max(0, startInStr))
+        endInStr = self.interval[1] - self.byteNum if self.interval[1] != -1 else strlen
+        endInStr = min(strlen, max(0, endInStr))
+
         self.byteNum += len(record)
-        if startInStr > strlen or endInStr <= startInStr:
-            return
-        self._output(record[startInStr:endInStr])
+        if endInStr - startInStr > 0:
+            self._output(record[startInStr:endInStr])
+        if self.byteNum > self.interval[1]:
+            self.eos()
+            self._shutdown()
 
 class BatchRecordDFN(DataflowNode):
     """Pass on records input->output in batches.  A batchsize of 0 means to
